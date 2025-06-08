0fc4428a1160529d42c33c40b61c9ce85d1970c9
ghost141-fd <ghost.fd141@gmail.com>
2025-06-08 13:26:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ async def send_all_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Главная функция
 def main():
-    app = ApplicationBuilder().token("7486297110:AAGJJfbEzgUcHmETvGjCZp-aRd-4T0V5qaI").build()
+    app = ApplicationBuilder().token(--------).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_niche))
